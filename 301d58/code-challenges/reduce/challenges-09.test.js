67fe59df1272d9f21f1feac52805773be5fd5c2e
Roger Huba <rogerhuba@gmail.com>
2019-05-03 13:31:44 -0700

    5 working

--- 301d58/code-challenges/reduce/challenges-09.test.js
+++ 301d58/code-challenges/reduce/challenges-09.test.js
@@ -162,13 +162,14 @@ Hint: The accumulator should begin as { count: 0, sum: 0 }
 ------------------------------------------------------------------------------------------------ */
 
 const calculateAverage = (arr) => {
-  return arr.reduce( (accumulator, element) => {
+  return arr.reduce( (accumulator, element, idx) => {
     accumulator.count ++;
     accumulator.sum += element;
-    console.log('count', accumulator.count, 'sum', accumulator.sum);
-    let avg = element.sum / element.count;
-    return accumulator;
-    return avg;
+    if (idx === arr.length-1) {
+      return (accumulator.sum / accumulator.count);
+    } else {
+      return accumulator;
+    }
   },{count: 0, sum: 0});
 };
 
