2fef11ebff61d7a991c2f60ef867c9f80e38f1bd
Roger Huba <charm --wait>
2019-06-12 19:52:52 -0700

    fix testing

--- 401d56/code-challenges/gradle/src/test/java/stacksandqueues/QueueTest.java
+++ 401d56/code-challenges/gradle/src/test/java/stacksandqueues/QueueTest.java
@@ -4,69 +4,88 @@ import static org.junit.Assert.*;
 
 
 public class QueueTest {
+
+    private Queue<Integer> makeQueue(){
+        Queue<Integer> newQueue = new Queue<>();
+        return newQueue;
+    }
+
+    private Queue<Integer> addNodesSingle(Queue newQueue){
+        newQueue.enQueue(5);
+        return newQueue;
+    }
+
+    private Queue<Integer> addNodesTwo(Queue newQueue){
+        newQueue.enQueue(5);
+        newQueue.enQueue(10);
+        return newQueue;
+    }
+
+    private Queue<Integer> addNodesMultiple(Queue newQueue){
+        newQueue.enQueue(10);
+        newQueue.enQueue(15);
+        newQueue.enQueue(20);
+        newQueue.enQueue(25);
+        newQueue.enQueue(30);
+        return newQueue;
+    }
+
     @Test
     public void testQueue(){
-        Queue newQueue = new Queue();
+        Queue<Integer> newQueue = makeQueue();
         assertNotNull("The new stack should not be null",newQueue);
     }
 
     @Test
     public void addToEmptyQueue(){
-        Queue newQueue = new Queue();
-        newQueue.enQueue(10);
-        assertEquals("The top should be 10", 10, newQueue.head.data);
+        Queue<Integer> newQueue = makeQueue();
+        addNodesSingle(newQueue);
+        int value = newQueue.head.data;
+        assertEquals("The top should be 5", 5, value);
     }
 
     @Test
     public void addMultipleToQueue(){
-        Queue newQueue = new Queue();
-        newQueue.enQueue(1);
-        newQueue.enQueue(15);
-        newQueue.enQueue(16);
-        newQueue.enQueue(19);
-        assertEquals("The top should be 10", 1, newQueue.head.data);
+        Queue<Integer> newQueue = makeQueue();
+        addNodesMultiple(newQueue);
+        int value = newQueue.head.data;
+        assertEquals("The top should be 10", 10, value);
     }
 
 
     @Test
     public void addToQueueMany(){
-        Queue newQueue = new Queue();
-        newQueue.enQueue(10);
-        newQueue.enQueue(18);
-        newQueue.enQueue(3988);
-        newQueue.enQueue(1);
-        assertEquals("The top should be 10", 10, newQueue.head.data);
+        Queue<Integer> newQueue = makeQueue();
+        addNodesMultiple(newQueue);
+        int value = newQueue.head.data;
+        assertEquals("The top should be 10", 10, value);
     }
 
     @Test
     public void removeFromQueuePopulatedPass(){
-        Queue newQueue = new Queue();
-        newQueue.enQueue(10);
-        newQueue.enQueue(13);
-        newQueue.enQueue(15);
-        newQueue.enQueue(19);
+        Queue<Integer> newQueue = makeQueue();
+        addNodesMultiple(newQueue);
         newQueue.deQueue();
         newQueue.deQueue();
         newQueue.deQueue();
-        assertEquals("The new top should be 3988", 19, newQueue.head.data);
+        int value = newQueue.head.data;
+        assertEquals("The new top should be 25", 25, value);
     }
 
     @Test
     public void removeFromQueuePopulatedFail(){
-        Queue newQueue = new Queue();
-        newQueue.enQueue(10);
-        newQueue.enQueue(13);
-        newQueue.enQueue(15);
-        newQueue.enQueue(19);
+        Queue<Integer> newQueue = makeQueue();
+        addNodesMultiple(newQueue);
         newQueue.deQueue();
         newQueue.deQueue();
         newQueue.deQueue();
-        assertNotEquals("The new top should be 3988", 15, newQueue.head.data);
+        int value = newQueue.head.data;
+        assertNotEquals("The new top should be 3988", 15, value);
     }
 
     @Test
     public void testIsQueueNotEmpty(){
-        Queue newQueue = new Queue();
+        Queue<Integer> newQueue = makeQueue();
         newQueue.enQueue(10);
         boolean empty = newQueue.isEmpty();
         assertFalse("The Queue is not empty", empty);
@@ -74,7 +93,7 @@ public class QueueTest {
 
     @Test
     public void testIsQueueEmpty(){
-        Queue newQueue = new Queue();
+        Queue<Integer> newQueue = makeQueue();
         boolean empty = newQueue.isEmpty();
         assertTrue("The Queue is not empty", empty);
     }
